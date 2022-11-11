39aafb66b5f808a63f8c6002f95652251a65d3cb
tyhon <47438855+tyhon@users.noreply.github.com>
2022-11-11 13:51:00 -0500

    using configuration and key vault managed identity

--- src/main/java/com/demo/keyvault/config/DataSourceConfig.java
+++ src/main/java/com/demo/keyvault/config/DataSourceConfig.java
@@ -1,28 +1,28 @@
-package com.demo.keyvault.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class DataSourceConfig {
-
-    @Value("spring-datasource-password")
-    private String datasourcePassword;
-    @Value("spring-datasource-username")
-    private String datasourceUsername;
-    @Value("spring-datasource-url")
-    private String datasourceUrl;
-
-    @Bean
-    public DataSource getDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url(datasourceUrl);
-        dataSourceBuilder.username(datasourceUsername);
-        dataSourceBuilder.password(datasourcePassword);
-        return dataSourceBuilder.build();
-    }
-}
+//package com.demo.keyvault.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class DataSourceConfig {
+//
+//    @Value("spring-datasource-password")
+//    private String datasourcePassword;
+//    @Value("spring-datasource-username")
+//    private String datasourceUsername;
+////    @Value("spring-datasource-url")
+//    private String datasourceUrl = "jdbc:postgresql://azuredemo.postgres.database.azure.com:5432/postgres";
+//
+//    @Bean
+//    public DataSource getDataSource() {
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.url(datasourceUrl);
+//        dataSourceBuilder.username(datasourceUsername);
+//        dataSourceBuilder.password(datasourcePassword);
+//        return dataSourceBuilder.build();
+//    }
+//}
